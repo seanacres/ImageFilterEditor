4670af0943c935d75e3f25e6120bdc919561656d
seanacres <6416690+seanacres@users.noreply.github.com>
2020-07-08 20:21:46 -0400

    Set up initial UI

---
+++ ImageFilterEditor/ImagePostViewController.swift
@@ -0,0 +1,35 @@
+//
+//  ImagePostViewController.swift
+//  ImageFilterEditor
+//
+//  Created by Sean Acres on 7/8/20.
+//  Copyright © 2020 Sean Acres. All rights reserved.
+//
+
+import UIKit
+
+class ImagePostViewController: UIViewController {
+
+    @IBOutlet weak var imageView: UIImageView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    @IBAction func choosePhotoPressed(_ sender: Any) {
+        
+    }
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
